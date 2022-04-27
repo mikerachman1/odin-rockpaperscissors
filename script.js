2590170b0bef13dbe8ed221866d618158d22c2d6
Mike Rachman <mikerachman1@gmail.com>
2022-04-27 16:15:14 -0400

    Created playRound function and game function added comments

--- script.js
+++ script.js
@@ -11,26 +11,51 @@ const computerPlay = () => {
     }
 }
 
-//this function evaluates the selections and returns the result of the round.
+// set variables to contain scores
+let computerScore = 0;
+let userScore = 0;
+
+//this function evaluates the selections and returns the result of the round and adds to scores. 
 const playRound = (playerSelection, computerSelection) => {
-    playerSelection = playerSelection.toLowerCase();
+    playerSelection = playerSelection.toLowerCase(); //turns playerselection into lowercase
     if (playerSelection == computerSelection) {
-        return 'The game is a tie!'
+        return 'The game is a tie!';
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        return 'The computer won! Paper beats rock.'
+        computerScore +=1;
+        return 'The computer won! Paper beats rock.';
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        userScore +=1;
         return 'You won! Rock beats scissors.'
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        userScore +=1;
         return 'You won! Paper beats rock.'
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        computerScore +=1;
         return 'The computer won! Scissors beats paper.'
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        computerScore +=1;
         return 'The computer won! Rock beats scissors'
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        userScore +=1;
         return 'You won! Scissors beats paper.'
     }
 }
 
-const playerSelection = 'RoCk';
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+
+const game = () => {
+    //loop through 5 rounds
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt('rock, paper, or scissors?', 'rock'); //pop up window to get playerselection
+        const computerSelection = computerPlay() //runs random choice generating function
+        console.log(playRound(playerSelection, computerSelection)); //logs results of round
+    }
+    //determine result of game
+    if (userScore > computerScore) {
+        console.log(`You won! The score is user: ${userScore}, computer: ${computerScore}`);
+    } else if (userScore < computerScore) {
+        console.log(`The computer won! The score is user: ${userScore}, computer: ${computerScore}`)
+    } else if (userScore == computerScore) {
+        console.log(`The game is a Tie! The score is user: ${userScore}, computer: ${computerScore}`)
+    }
+}
+game(); //run game function
